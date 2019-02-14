fd9350431f33d514dd374a96e620cc8b2d83d51a
tgray017 <tgray017@gmail.com>
2019-02-14 13:30:24 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,6 +1,6 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
   def change
-    drop_table 
+    drop_table :artists
     create_table :artists do |t|
       t.string :name
       t.string :genre
